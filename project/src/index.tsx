33aee11c8039879226bd250de395f5f16a63e8bf
Anastasiia <nastyara13@yandex.ru>
2023-03-02 23:03:38 +0300

    Добавлено отображение карточек

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app/app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const Setting = {
   offersCount: 6,
 } as const;
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
