ce50c077634e1bc0e517f6d82f328bec101b1b59
Anastasiia <nastyara13@yandex.ru>
2023-03-04 21:59:59 +0300

    Добавлена стилизация 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,31 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+
 import Home from '../../pages/home/home';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import { AppRoute } from '../../const/const';
 
 type AppScreenProps = {
   offersCount: number;
 };
 
-function App({ offersCount }: AppScreenProps): JSX.Element {
-  return <Home offersCount={offersCount} />;
-}
+const App: React.FC<AppScreenProps> = ({ offersCount }) => (
+  <HelmetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Home offersCount={offersCount} />}
+        />
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Property} element={<Property />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </HelmetProvider>
+);
 
 export default App;
--- project/src/pages/property/property.tsx
+++ project/src/pages/property/property.tsx
@@ -1,44 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Logo from '../../components/logo/logo';
 
 const Property: React.FC = () => (
   <div className="page">
-    <div style={{ display: 'none' }}>
-      <svg xmlns="http://www.w3.org/2000/svg">
-        <symbol id="icon-arrow-select" viewBox="0 0 7 4">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"
-          >
-          </path>
-        </symbol>
-        <symbol id="icon-bookmark" viewBox="0 0 17 18">
-          <path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path>
-        </symbol>
-        <symbol id="icon-star" viewBox="0 0 13 12">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"
-          >
-          </path>
-        </symbol>
-      </svg>
-    </div>
-
+    <Helmet>
+      <title>Six Cities. Property</title>
+    </Helmet>
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link" href="main.html">
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width="81"
-                height="41"
-              />
-            </a>
+            <Logo />
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
@@ -46,7 +19,7 @@ const Property: React.FC = () => (
                 <div className="header__nav-profile">
                   <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                   <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
+                    Oliver.conner@gmail.com
                   </span>
                 </div>
               </li>
@@ -116,7 +89,7 @@ const Property: React.FC = () => (
             </div>
             <div className="property__name-wrapper">
               <h1 className="property__name">
-                  Beautiful &amp; luxurious studio at great location
+                Beautiful &amp; luxurious studio at great location
               </h1>
             </div>
             <div className="property__rating rating">
@@ -124,19 +97,17 @@ const Property: React.FC = () => (
                 <span style={{ width: '80%' }}></span>
                 <span className="visually-hidden">Rating</span>
               </div>
-              <span className="property__rating-value rating__value">
-                  4.8
-              </span>
+              <span className="property__rating-value rating__value">4.8</span>
             </div>
             <ul className="property__features">
               <li className="property__feature property__feature--entire">
-                  Apartment
+                Apartment
               </li>
               <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                3 Bedrooms
               </li>
               <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                Max 4 adults
               </li>
             </ul>
             <div className="property__price">
@@ -175,20 +146,20 @@ const Property: React.FC = () => (
               </div>
               <div className="property__description">
                 <p className="property__text">
-                    A quiet cozy and picturesque that hides behind a a river by
-                    the unique lightness of Amsterdam. The building is green and
-                    from 18th century.
+                  A quiet cozy and picturesque that hides behind a a river by
+                  the unique lightness of Amsterdam. The building is green and
+                  from 18th century.
                 </p>
                 <p className="property__text">
-                    An independent House, strategically located between Rembrand
-                    Square and National Opera, but where the bustle of the city
-                    comes to rest in this alley flowery and colorful.
+                  An independent House, strategically located between Rembrand
+                  Square and National Opera, but where the bustle of the city
+                  comes to rest in this alley flowery and colorful.
                 </p>
               </div>
             </div>
             <section className="property__reviews reviews">
               <h2 className="reviews__title">
-                  Reviews &middot; <span className="reviews__amount">1</span>
+                Reviews &middot; <span className="reviews__amount">1</span>
               </h2>
               <ul className="reviews__list">
                 <li className="reviews__item">
@@ -212,22 +183,19 @@ const Property: React.FC = () => (
                       </div>
                     </div>
                     <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river
-                        by the unique lightness of Amsterdam. The building is
-                        green and from 18th century.
+                      A quiet cozy and picturesque that hides behind a a river
+                      by the unique lightness of Amsterdam. The building is
+                      green and from 18th century.
                     </p>
                     <time className="reviews__time" dateTime="2019-04-24">
-                        April 2019
+                      April 2019
                     </time>
                   </div>
                 </li>
               </ul>
               <form className="reviews__form form" action="#" method="post">
-                <label
-                  className="reviews__label form__label"
-                  htmlFor="review"
-                >
-                    Your review
+                <label className="reviews__label form__label" htmlFor="review">
+                  Your review
                 </label>
                 <div className="reviews__rating-form form__rating">
                   <input
@@ -324,9 +292,9 @@ const Property: React.FC = () => (
                 </textarea>
                 <div className="reviews__button-wrapper">
                   <p className="reviews__help">
-                      To submit review please make sure to set{' '}
+                    To submit review please make sure to set{' '}
                     <span className="reviews__star">rating</span> and describe
-                      your stay with at least{' '}
+                    your stay with at least{' '}
                     <b className="reviews__text-amount">50 characters</b>.
                   </p>
                   <button
@@ -334,7 +302,7 @@ const Property: React.FC = () => (
                     type="submit"
                     disabled
                   >
-                      Submit
+                    Submit
                   </button>
                 </div>
               </form>
@@ -346,7 +314,7 @@ const Property: React.FC = () => (
       <div className="container">
         <section className="near-places places">
           <h2 className="near-places__title">
-              Other places in the neighbourhood
+            Other places in the neighbourhood
           </h2>
           <div className="near-places__list places__list">
             <article className="near-places__card place-card">
@@ -366,7 +334,7 @@ const Property: React.FC = () => (
                   <div className="place-card__price">
                     <b className="place-card__price-value">&euro;80</b>
                     <span className="place-card__price-text">
-                        &#47;&nbsp;night
+                      &#47;&nbsp;night
                     </span>
                   </div>
                 </div>
@@ -400,7 +368,7 @@ const Property: React.FC = () => (
                   <div className="place-card__price">
                     <b className="place-card__price-value">&euro;132</b>
                     <span className="place-card__price-text">
-                        &#47;&nbsp;night
+                      &#47;&nbsp;night
                     </span>
                   </div>
                 </div>
@@ -437,7 +405,7 @@ const Property: React.FC = () => (
                   <div className="place-card__price">
                     <b className="place-card__price-value">&euro;180</b>
                     <span className="place-card__price-text">
-                        &#47;&nbsp;night
+                      &#47;&nbsp;night
                     </span>
                   </div>
                 </div>
