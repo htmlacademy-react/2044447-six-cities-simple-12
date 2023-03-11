d85ee2eefdbe912ed33d9c5dc67d8b055037cd98
Anastasiia <nastyara13@yandex.ru>
2023-03-11 14:55:51 +0300

    Добавлена карта

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -26,7 +26,6 @@ const Header: React.FC<HeaderProps> = ({ hasNav = true }) => (
                   <span className="header__user-name user__name">
                     Oliver.conner@gmail.com
                   </span>
-                  <span className="header__favorite-count">3</span>
                 </Link>
               </li>
               <li className="header__nav-item">
