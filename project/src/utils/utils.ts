cf0b101783c95bf27082ae1c2dcde107e22ca189
Anastasiia <nastyara13@yandex.ru>
2023-03-13 13:20:28 +0300

    Добавлены отзывы

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,2 +1,7 @@
+import dayjs from 'dayjs';
+
 export const getRatingColor = (rating: number) =>
   (Math.round(rating) * 100) / 5;
+
+export const humanizeDate = (date: string, format: string) =>
+  dayjs(date).format(format);
