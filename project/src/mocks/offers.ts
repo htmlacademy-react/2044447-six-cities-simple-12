adda384eedd228ce6cde7c74906954fdb4840fef
Anastasiia <nastyara13@yandex.ru>
2023-03-14 16:40:07 +0300

    Добавлен редакс

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -81,7 +81,7 @@ export const offers: Offer[] = [
         longitude: 4.895168,
         zoom: 10,
       },
-      name: 'Amsterdam',
+      name: 'Hamburg',
     },
     description:
       'ADuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
@@ -115,7 +115,7 @@ export const offers: Offer[] = [
         longitude: 4.895168,
         zoom: 10,
       },
-      name: 'Amsterdam',
+      name: 'Paris',
     },
     description:
       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis',
